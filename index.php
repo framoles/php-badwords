9e1477a1fa179f6c7456f59d1883a3f60676cc8f
Francesco Moles <francesco.moles@outlook.it>
2022-05-02 16:01:47 +0200

    Creazione variabile + assegnazione stringa, stampa variabile, stampa lunghezza variabile

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-badwords</title>
+    
+</head>
+<body>
+<?php
+
+$par = "Dio computer, che sei nei desktop, sia santificato il tuo socket";
+
+
+?>
+<h2>Paragrafo:</h2>
+<p><?php echo ($par);?></p>
+<h2>Lunghezza del paragrafo:</h2>
+<p><?php echo (strlen($par));?></p>
+</body>
+</html>
