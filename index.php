2d6dafff800bdc200647b2f08dec354fb41b70f7
Francesco Moles <francesco.moles@outlook.it>
2022-05-02 18:34:18 +0200

    aggiunta parola da censurare tramite parametro get

--- index.php
+++ index.php
@@ -12,11 +12,22 @@
 
 $par = "Dio computer, che sei nei desktop, sia santificato il tuo socket";
 
+$bad_word = $_GET["badword"];
+
+$par2 = str_replace($bad_word,"***",$par);
+
 
 ?>
 <h2>Paragrafo:</h2>
 <p><?php echo ($par);?></p>
 <h2>Lunghezza del paragrafo:</h2>
 <p><?php echo (strlen($par));?></p>
+<!-- censura -->
+<p>-----------------------------------</p>
+<h2>Paragrafo censurato:</h2>
+<p><?php echo ($par2);?></p>
+<h2>Lunghezza del paragrafo censuratoS:</h2>
+<p><?php echo (strlen($par2));?></p>
+<!-- fine censura -->
 </body>
 </html>
